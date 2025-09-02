525350111e9c7331142acda6c21b323c5e0ebb2d
vikas2903 <prasadvvikas@gmail.com>
2025-09-02 15:11:57 +0530

    Deployment

---
+++ app/models/bxgyconfig.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const BxgyConfigSchema = new mongoose.Schema(
+  {
+    shopName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    enabled: {
+      type: Boolean,
+      default: false,
+    },
+    offer: {
+      buyQty: { type: Number, required: true, min: 1 },
+      getQty: { type: Number, required: true, min: 1 },
+    },
+    messages: {
+      remainingMany: { type: String, required: true },
+      remainingOne: { type: String, required: true },
+      unlocked: { type: String, required: true },
+    },
+  },
+  { timestamps: true },
+);
+
+export const BxgyConfig =
+  mongoose.models.BxgyConfig || mongoose.model("BxgyConfig", BxgyConfigSchema);
--- app/shopify.server.js
+++ app/shopify.server.js
@@ -5,10 +5,8 @@ import {
   shopifyApp,
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-import prisma from "./db.server";
-// import connectDatabase from '../app/lib/dbconnect.js'
+import prisma from "./db.server.js";
 
-// connectDatabase();
 
 
 const shopify = shopifyApp({
